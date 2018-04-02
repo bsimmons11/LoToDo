97de145c97b9c27c7f7fbc7b949b3ad439a00a38
Brady Simmons <bsimmons@lorux.com>
2018-04-01 21:29:28 -0500

    Convert to MVC design pattern

---
+++ LoToDo/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  LoToDo
+//
+//  Created by Brady Simmons on 4/1/18.
+//  Copyright © 2018 Brady Simmons. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
